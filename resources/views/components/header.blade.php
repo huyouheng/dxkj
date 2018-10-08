47babb40966f76df6cc390253b13120e93be325e
huyouheng <876697515@qq.com>
2018-10-08 19:39:46 +0800

    remove logout

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -18,21 +18,11 @@
             <ul class="nav navbar-nav">
 
                 <li class="dropdown user user-menu">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                    <a href="#">
                         <img src="{{url('/dist/img/user2-160x160.jpg')}}" class="user-image" alt="User Image">
                         <span class="hidden-xs">@auth {{Auth::user()->name}} @endauth</span>
                     </a>
-                    <ul class="dropdown-menu">
-                        <!-- Menu Footer-->
-                        <li class="user-footer">
-                            <div class="">
-                                <a href="" class="btn btn-default btn-flat">退出</a>
-                            </div>
-                            <div class="pull-right">
 
-                            </div>
-                        </li>
-                    </ul>
                 </li>
             </ul>
         </div>
